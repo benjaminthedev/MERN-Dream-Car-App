20be4c773a7cb8bea4e94ac0f91c2a22eb310423
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2021-01-03 11:02:34 +0000

    updated the project

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,6 +1,19 @@
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles';
 
-
-export default makeStyles(() => {
-
-});
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 5,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '20px',
+  },
+  heading: {
+    color: '#000',
+  },
+  image: {
+    marginLeft: '15px',
+  },
+}));
